fb2ad2503b8b513a1b572854b7fa445b8db6afa3
silvia <surtle@me.com>
2019-01-29 00:06:25 -0800

    added places and faces page

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ var description = document.getElementById('description');
 var portrait = document.getElementById('portrait');
 var landscape = document.getElementById('landscape');
 
+landscape.onclick = function() {
+  window.location.replace("places.html");
+}
+
+portrait.onclick = function() {
+  window.location.replace("faces.html"); 
+}
+
 banner_txt.onclick = function() {
   while(description.style.opacity < 1) {
     description.style.opacity = description.style.opacity + 0.1;
